79321987b8d4c3e2c91904892ee2e4134ea8a18d
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-05 21:34:18 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,9 +7,9 @@ def hello_t(array)
     yield (array[i])
     i = i + 1
   end
-  
-  array
-else
-  puts "Hey! No block was given!"
-end
 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
